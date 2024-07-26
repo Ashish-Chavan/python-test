a52d6edfffbc1caf0f945c63e13bbe8b0226c4e4
Ashish-Chavan <110707870+Ashish-Chavan@users.noreply.github.com>
2024-07-26 21:07:00 +0530

    Update File1.py

--- File1.py
+++ File1.py
@@ -16,11 +16,6 @@ def multiply(num1, num2):
 def divide(num1, num2):
 	return num1 / num2
 
-print("Please select operation -\n" \
-		"1. Add\n" \
-		"2. Subtract\n" \
-		"3. Multiply\n" \
-		"4. Divide\n")
 
 
 # Take input from the user
@@ -32,14 +27,6 @@ number_2 = int(input("Enter second number: "))
 if select == 1:
 	print(number_1, "+", number_2, "=",
 					add(number_1, number_2))
-
-elif select == 2:
-	print(number_1, "-", number_2, "=",
-					subtract(number_1, number_2))
-
-elif select == 3:
-	print(number_1, "*", number_2, "=",
-					multiply(number_1, number_2))
 
 elif select == 4:
 	print(number_1, "/", number_2, "=",
